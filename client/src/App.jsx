024888733b26b3a12691413f009d954c3b41690e
Sid Sharma <sharma.sidhant@outlook.com>
2024-06-21 18:41:20 -0400

    Test again

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Navigation from './customer/components/Navigation/Navigation'
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      Test
+    </div>
+  )
+}
+
+export default App
